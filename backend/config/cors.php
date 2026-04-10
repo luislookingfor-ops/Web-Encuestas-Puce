d47768de901585db15c2bbe52227046ed1322207
JORGE LUIS <jlcorrales@puce.edu.ec>
2026-04-09 20:10:48 -0500

    arreglo en nixpacks

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+    ],
 
     'allowed_origins_patterns' => [],
 
