49a3c5890e194f12c78024511d2b2e70bf60d4a4
Jorge Luis Corrales <luislookingfor@gmail.com>
2026-05-14 23:39:37 -0500

    Remove throttle middleware from login route
    Removed throttle middleware from login route.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,9 +11,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::post('/login', [AuthController::class, 'login'])
-    ->middleware('throttle:5,1')
-    ->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/questions', [QuestionController::class, 'index']);
 
